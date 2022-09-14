15b0fc69e5143ae53631851bd61f0a91e2d96e13
Francisco Mora <fmorajara@gmail.com>
2022-09-14 17:56:47 -0300

    Tailwind añadido
    Se añadió Tailwind, solo hace falta hacer la prueba e iniciarlo localmente

---
+++ static/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './web/styles/*.css',
+    './web/scripts/*.js',
+    './web/artist/*.html',
+    './web/artlover/*.html',
+    './static/web/public/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
