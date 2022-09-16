135e9e84e9c7a2cd8110a7187c7eee135bba6891
Francisco Mora <fmorajara@gmail.com>
2022-09-15 22:08:10 -0300

    Configuraciones de variables para colores

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -10,7 +10,46 @@ module.exports = {
     './web/public/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin:{
+          base: withOpacity('--color-text-base'),
+          inverted: withOpacity('--color-text-inverted'),
+          muted: withOpacity('--color-text-muted'),
+          accent: withOpacity('--color-text-accent')
+        }
+      },
+      backgroundColor:{
+        skin:{
+          normal: withOpacity('--color-normal'),
+          base: withOpacity('--color-button-base'),
+          accent: withOpacity('--color-button-accent'),
+          muted: withOpacity('--color-button-muted'),
+          disabled: withOpacity('--color-button-disabled')
+        }
+      },
+      gradientColorStops:{
+        stop:{
+          normal: withOpacity('--color-normal'),
+          base: withOpacity('--color-button-base'),
+          accent: withOpacity('--color-button-accent'),
+          muted: withOpacity('--color-button-muted'),
+          disabled: withOpacity('--color-button-disabled')
+        }
+      },
+      fontFamily: {
+        'bahnschrift' : ['Bahnschrift','sans-serif'],
+      }
+    },
   },
   plugins: [],
+}
+
+function withOpacity(variable){
+  return ({opacityValue}) => {
+    if (opacityValue != undefined){
+      return `rgba(var(${variable}),${opacityValue})`;
+    }
+    return `rgb(var(${variable}))`;
+  }
 }
