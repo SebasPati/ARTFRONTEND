c4b5fb45a3feefe50feb99ca8ee5d7c039c287d2
clau1069 <claudiaruiz1069@gmail.com>
2022-09-16 13:05:27 -0300

    cambios index

--- static/web/scripts/index.js
+++ static/web/scripts/index.js
@@ -2,24 +2,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            tema: '',
         }
     },
     created() {
-        this.initialTheme();
     },
     mounted() {
     },
     methods: {
-        theme(a) {
-            this.tema = `tema_${a}`;
-            localStorage.setItem('theme', JSON.stringify(a));
-        },
-        initialTheme() {
-            if (JSON.parse(localStorage.getItem('theme'))) {
-                this.theme(JSON.parse(localStorage.getItem('theme')))
-            }
-        },
     },
     computed: {
     }
